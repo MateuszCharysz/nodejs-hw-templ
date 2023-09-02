a7b48cb782fe273e9950754e0ea6a2a49126ca58
MateuszCharysz <charyszek@interia.pl>
2023-09-02 13:32:03 +0200

    druga próba poprawy

--- middleweres/jwtStrategy.js
+++ middleweres/jwtStrategy.js
@@ -15,8 +15,8 @@ passport.use(
   new Strategy(params, async (payload, done) => {
     const user = await findUserForToken(payload.id);
     try {
-      if (!user && user._id.toString() === payload.id) {
-        return done(new Error('User not found or token isnt right'));
+      if (!user) {
+        return done(new Error('User not found'));
       } else {
         return done(null, user);
       }
@@ -27,9 +27,14 @@ passport.use(
 );
 
 const auth = async (req, res, next) => {
-  await passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (!user || err) {
-      return res.status(401).json({ message: 'Not authorized' });
+  const token = req.headers.authorization?.split(' ')[1];
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user || !user.token || user.token !== token) {
+      return res.status(401).json({
+        status: 'fail',
+        code: 401,
+        message: 'Not authorized',
+      });
     }
     req.user = user;
     next();
